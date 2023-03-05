b41dd5890faca05e0b237fef52f93c0eb2ef799e
Matheus Victor <matheusvictor.salles@gmail.com>
2023-03-05 11:00:36 -0300

    Adiciona comentários

--- src/main/java/br/com/dio/pilha/Pilha.java
+++ src/main/java/br/com/dio/pilha/Pilha.java
@@ -2,7 +2,7 @@ package br.com.dio.pilha;
 
 public class Pilha {
 
-    private No refNoEntrada;
+    private No<Integer> refNoEntrada;
 
     public Pilha() {
         this.refNoEntrada = null;
@@ -12,23 +12,26 @@ public class Pilha {
         return refNoEntrada == null;
     }
 
-    public No top() {
+    public No<Integer> top() {
         return this.refNoEntrada;
     }
 
-    public void push(No novoNo) {
-        No refNoAuxiliar = this.refNoEntrada; // guarda a referência do topo em um Nó auxiliar
+    public void push(No<Integer> novoNo) {
+        No<Integer> refNoAuxiliar = this.refNoEntrada; // guarda a referência do topo em um Nó auxiliar
         this.refNoEntrada = novoNo; // a referência de entrada passa a ser o novo Nó que foi inserido
+
+        // a referência de próximo do novo Nó passa a ser o Nó anterior, cuja ref. está guardada na variável auxiliar refNoAuxiliar:
         novoNo.setRefProximoNo(refNoAuxiliar);
     }
 
-    public No pop() {
-        if (!this.isEmpty()) {
-            No noRetirado = this.refNoEntrada;
-            this.refNoEntrada = this.refNoEntrada.getRefProximoNo();
-            return noRetirado;
-        }
-        return null;
-    }
+    public No<Integer> pop() {
+        if (!this.isEmpty()) { // a remoção só irá ocorrer caso haja presença de algum Nó na pilha
+            No<Integer> noRetirado = this.refNoEntrada; // guarda a referência do Nó que será removido
 
+            // o Nó de entrada passa a ser o Nó que está imediatamente abaixo do Nó que será removido
+            this.refNoEntrada = this.refNoEntrada.getRefProximoNo();
+            return noRetirado; // retorna a referência do Nó removido
+        }
+        return null; // caso a pilha esteja vazia, deverá retornar um ponteiro nulo
+    }
 }
