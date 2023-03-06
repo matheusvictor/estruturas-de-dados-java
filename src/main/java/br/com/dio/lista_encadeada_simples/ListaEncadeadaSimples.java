1c66eb3c54aef73fe5e0a19d276e21d856f09de7
Matheus Victor <matheusvictor.salles@gmail.com>
2023-03-06 08:09:39 -0300

    wip: Add classe de lista encadeada simples

---
+++ src/main/java/br/com/dio/lista_encadeada_simples/ListaEncadeadaSimples.java
@@ -0,0 +1,91 @@
+package br.com.dio.lista_encadeada_simples;
+
+import br.com.dio.no.No;
+
+public class ListaEncadeadaSimples<T> {
+
+    private No<T> noRefEntrada;
+
+    public ListaEncadeadaSimples() {
+        this.noRefEntrada = null;
+    }
+
+    public int size() {
+        int tamanhoLista = 0;
+
+        No<T> refAuxiliar = this.noRefEntrada;
+
+        while (true) {
+            if (refAuxiliar != null) {
+                tamanhoLista++;
+                if (refAuxiliar.getProximoNo() != null) {
+                    refAuxiliar = refAuxiliar.getProximoNo();
+                }
+            } else break;
+        }
+
+        return tamanhoLista;
+    }
+
+    public boolean isEmpty() {
+        return this.noRefEntrada == null;
+    }
+
+    public void add(T conteudo) {
+        No<T> novoNo = new No<>(conteudo);
+
+        // se a lista estiver vazia, o novoNo será adicionado e será a ref. de entrada na lista
+        if (this.isEmpty()) {
+            this.noRefEntrada = novoNo;
+            return;
+        }
+
+        No<T> noAuxiliar = this.noRefEntrada;
+        for (int i = 0; i < (this.size() - 1); i++) {
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+
+        // quando o próximo Nó de noAuxiliar for nulo, deve passar a a pontar para novoNovo e este, por sua vez, deverá a pontar para nulo
+        noAuxiliar.setProximoNo(novoNo);
+    }
+
+    public T remove(int indice) {
+        No<T> noARemover = this.getNo(indice);
+
+        // caso esteja removendo o primeiro Nó da lista...
+        if (indice == 0) {
+            this.noRefEntrada = noARemover.getProximoNo(); // o nó de entrada passa a ser o Nó imediatamente posterior ao Nó que será removido
+        } else {
+            No<T> noAnterior = getNo(indice - 1);
+            noAnterior.setProximoNo(noARemover.getProximoNo());
+        }
+        return noARemover.getConteudo();
+    }
+
+    public T get(int indice) {
+        return this.getNo(indice).getConteudo();
+    }
+
+    private No<T> getNo(int indice) {
+
+        validaIndice(indice);
+
+        No<T> no = null;
+        No<T> noAuxiliar = this.noRefEntrada;
+
+        if (!this.isEmpty()) {
+            for (int i = 0; i < (this.size() - 1); i++) {
+                no = noAuxiliar;
+                noAuxiliar = noAuxiliar.getProximoNo();
+            }
+        }
+        return no;
+    }
+
+    private void validaIndice(int indice) throws IndexOutOfBoundsException {
+        if (indice >= this.size()) {
+            throw new IndexOutOfBoundsException("Essa lista não possui conteúdo no índice " + indice);
+        }
+    }
+
+}
